54a9b3c3ef83d84ebdc43b4a71152798eab69218
BT_1N3_02 <BT_1N3_02@BT_1N3_02.bktech.local>
2017-12-01 13:12:57 -0500

    methods initialized

---
+++ Lab 3.4/src/ArrayMethods4.java
@@ -0,0 +1,24 @@
+
+public class ArrayMethods4
+{
+	
+	public static void main(String[] args)
+	{
+		System.out.println();
+	}
+	
+	public static int partition(int[] list1, int front, int back)
+	{
+		
+	}
+	
+	public static void quickSort(int[] list1, int front, int back)
+	{
+		
+	}
+	
+	public static void customSort(whatever)
+	{
+		
+	}
+}
